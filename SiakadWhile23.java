70f1094456e8af003946a0a6afdc6ec1176007fc
reffi igamaiyoda <email_gitbuh_igmaiyoda10@gmail.com>
2025-10-23 22:54:46 +0700

    Update SiakadWhile

--- SiakadWhile23.java
+++ SiakadWhile23.java
@@ -21,19 +21,19 @@ public class SiakadWhile23 {
                 }
             }
             if (nilai > 80 && nilai <= 100) {
-                System.out.println("Nilai Mahasiswa ke-" + (i + 1) + "Adalah A");
+                System.out.println("Nilai Mahasiswa ke- " + (i + 1) + " Adalah A" + " Bagus, Pertahankan Nilainya");
             } else if (nilai > 73 && nilai <= 80) {
-                System.out.println("Nilai Mahasiswa ke-" + (i + 1) + "Adalah B+");
+                System.out.println("Nilai Mahasiswa ke- " + (i + 1) + " Adalah B+");
             } else if (nilai > 65 && nilai <= 73) {
-                System.out.println("Nilai Mahasiswa ke-" + (i + 1) + "Adalah B");
+                System.out.println("Nilai Mahasiswa ke- " + (i + 1) + " Adalah B");
             } else if (nilai > 60 && nilai <= 65) {
-                System.out.println("Nilai Mahasiswa ke-" + (i + 1) + "Adalah C+");
+                System.out.println("Nilai Mahasiswa ke- " + (i + 1) + " Adalah C+");
             } else if (nilai > 50 && nilai <= 60) {
-                System.out.println("Nilai Mahasiswa ke-" + (i + 1) + "Adalah C");
+                System.out.println("Nilai Mahasiswa ke- " + (i + 1) + " Adalah C");
             } else if (nilai > 39 && nilai <= 50) {
-                System.out.println("Nilai Mahasiswa ke-" + (i + 1) + "Adalah D");
+                System.out.println("Nilai Mahasiswa ke- " + (i + 1) + " Adalah D");
             } else {
-                System.out.println("Nilai Mahasiswa ke-" + (i + 1) + "Adalah E");
+                System.out.println("Nilai Mahasiswa ke- " + (i + 1) + " Adalah E");
             }
             i++;
 
